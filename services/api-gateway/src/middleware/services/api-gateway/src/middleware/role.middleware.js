c18819ce105da2f9360325e38d54806bd04c180e
Devi226 <gdeviprasanna595@gmail.com>
2026-09-01 20:27:07 +0530

    Delete services/api-gateway/src/middleware/services/api-gateway/src/middleware/role.middleware.js

--- services/api-gateway/src/middleware/services/api-gateway/src/middleware/role.middleware.js
+++
@@ -1,9 +0,0 @@
-const authorizeRole = (roles) => {
-  return (req, res, next) => {
-    if (!roles.includes(req.user.role)) {
-      return res.status(403).json({ error: "Forbidden: Insufficient role" });
-    }
-    next();
-  };
-};
-module.exports = authorizeRole;
